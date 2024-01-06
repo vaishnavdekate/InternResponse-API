ac3d6270e4792c8e04ff3f499bf42c93b78c2b4a
vaishnavdekate <155634424+vaishnavdekate@users.noreply.github.com>
2024-01-06 11:33:49 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,26 @@
-const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/InternshipRegistrationFormResponses", {
-    
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("Connected to MongoDB");
-}).catch((e) => {
-    console.log("Connection Failed:", e.message);
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://vaishnavdekate2002:<password>@intern-db.zmvasmz.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
